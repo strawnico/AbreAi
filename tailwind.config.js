42c1121f277b4fecb8b9ffd828ec246dc3a8229f
Strawnico <nicolelicyanne0@gmail.com>
2022-12-08 15:11:32 -0300

    finish home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
